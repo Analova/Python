6a9b2f323dd532ee7e21ad459bcd9088b55dbc57
Ana Nikolova <ani.nik19@gmail.com>
2021-01-08 17:16:00 +0100

    class attributes vs instance attributes

--- classes.py
+++ classes.py
@@ -4,10 +4,19 @@ class Car:
         self.model=model,
         self.wheels=wheels,
         self.windows=windows,
-        print(f"Created instance of a car for {maker} {model}")
+        self.total=5
+        # print(f"Created instance of a car for {maker} {model}")
+
+     
+     total=20
 
      def printMaker(self):
-        return self.maker
+         return self.maker
+
+     def add(self,number):
+         Car.total+=number
+         self.total+=number
+         return f"Class:{Car.total} Instance:{self.total}"
 
 
 car1=Car("Benz", "ML250")
@@ -15,5 +24,5 @@ car2=Car("Benz", "ML250")
 car3=Car("BNW", "x6",4,5)
 car4=Car("Honda", "Civic Coupe",4,4)
 
-print(car3.printMaker())
+print(car1.add(10))
 
