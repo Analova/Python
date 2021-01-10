839b3a828a93d8d79c050e63d210f41ff6178c5e
Ana Nikolova <ani.nik19@gmail.com>
2021-01-10 16:02:13 +0100

    convert JSON Data to Dictionary

--- pokemon.py
+++ pokemon.py
@@ -1,5 +1,7 @@
 import requests
+import json
 
 data=requests.get("https://pokeapi.co/api/v2/pokemon/ditto")
+data =json.loads(data.text)
 
-print(data.json())
+print(data["moves"][0]["move"]["name"])
