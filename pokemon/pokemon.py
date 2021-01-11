a775f0a79ad8a3ee414780c2e2bcb6207d1fda36
Ana Nikolova <ani.nik19@gmail.com>
2021-01-11 11:25:15 +0100

    display pokemon info

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -12,4 +12,15 @@ print(f"You choose {data['name']}")
 
 action = input("What informtaion would you like to see to see?b(i=info , s=stats) ")
 
-print(data["moves"][0]["move"]["name"])
+if action == "i" or action == "info":
+    print(f"id:{data['id']}")
+    print(f"name:{data['name']}")
+    print(f"height:{data['height']}")
+    print(f"weight:{data['weight']}")
+    pokemon_type=""
+    for type in data["types"]:
+        if data["types"].index(type) == (len(data["types"])-1):
+            pokemon_type+= f"{type['type']['name']}"
+        else:
+            pokemon_type+= f"{type['type']['name']}, "
+    print(f"type:{pokemon_type}")   
